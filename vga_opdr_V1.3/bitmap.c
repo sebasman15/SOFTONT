11281ce803f44b10520b0cb67ba5404a8c41f1dd
JorikWiegman <jorik.wiegman@student.hu.nl>
2017-05-16 18:58:09 +0200

    Add files via upload

--- vga_opdr_V1.3/bitmap.c
+++ vga_opdr_V1.3/bitmap.c
@@ -1,31 +1,131 @@
 #include "stm32_ub_vga_screen.h"
-typedef enum{omhoog,omlaag,links,rechts};
+#include <bitmap.h>
+//typedef enum richting{omhoog,omlaag,links,rechts};
 
-void smiley()
+void smiley(int x_loc, int y_loc)
 {
-	int smiley_array[279][339];
+	int yt;
+	int xt;
+	int i = 0;
+	for(yt=0;yt<100;yt++)
+	{
+		for(xt=0;xt<100;xt++)
+		{
+			if (SMILEY[i] != 0)
+			{
+			  UB_VGA_SetPixel(xt+x_loc,yt+y_loc,SMILEY[i]);
+			}
+			i++;
+		}
+	}
 }
-void pijl(int richting)
+void pijl(int x_loc, int y_loc, int richting)
 {
-	int pijl_array_omhoog[279][339];
-	int pijl_array_omlaag[279][339];
-	int pijl_array_rechts[279][339];
-	int pijl_array_links[279][339];
-
 	switch (richting) {
 		case omhoog:
-
+			pijl_up(x_loc, y_loc);
 			break;
 		case omlaag:
-
+//			Pijl_down(x_loc, y_loc);
 			break;
 		case rechts:
-
+//			pijl_right(x_loc,y_loc);
 			break;
 		case links:
-
+//			pijl_left(x_loc,y_loc);
 			break;
 		default:
 			break;
+	}
+}
+
+void pijl_up(int x_loc, int y_loc, int colour)
+{
+	int yt;
+	int xt;
+	int i = 0;
+	for(yt=0;yt<120;yt++)
+	{
+		for(xt=0;xt<65;xt++)
+		{
+			if (PIJL_UP[i] != 0)
+			{
+			  UB_VGA_SetPixel(xt+x_loc,yt+y_loc,colour);
+			}
+			i++;
+		}
+	}
+}
+
+void pijl_down(int x_loc, int y_loc, int colour)
+{
+	int yt;
+	int xt;
+	int i = 0;
+	for(yt=0;yt<120;yt++)
+	{
+		for(xt=0;xt<65;xt++)
+		{
+			if (PIJL_DOWN[i] != 0)
+			{
+			  UB_VGA_SetPixel(xt+x_loc,yt+y_loc,colour);
+			}
+			i++;
+		}
+	}
+}
+
+void pijl_left(int x_loc, int y_loc, int colour)
+{
+	int yt;
+	int xt;
+	int i = 0;
+	for(yt=0;yt<65;yt++)
+	{
+		for(xt=0;xt<120;xt++)
+		{
+			if (PIJL_LEFT[i] != 0)
+			{
+			  UB_VGA_SetPixel(xt+x_loc,yt+y_loc,colour);
+			}
+			i++;
+		}
+	}
+}
+
+void pijl_right(int x_loc, int y_loc, int colour)
+{
+	int yt;
+	int xt;
+	int i = 0;
+	for(yt=0;yt<65;yt++)
+	{
+		for(xt=0;xt<120;xt++)
+		{
+			if (PIJL_RIGHT[i] != 0)
+			{
+			  UB_VGA_SetPixel(xt+x_loc,yt+y_loc,colour);
+			}
+			i++;
+		}
+	}
+}
+
+void Katja()
+{
+	int yt;
+	int xt;
+	int y_loc = 0;
+	int x_loc = 0;
+	int TA = 0;
+	int val;
+	int *p_bit;
+	for(yt=0;yt<142;yt++)
+	{
+		for(xt=0;xt<100;xt++)
+		{
+			UB_VGA_SetPixel(xt+x_loc,yt+x_loc,KATJA[TA]);
+			TA++;
+		}
 	}
 }
